f4be8f58468b738bf6dbf91ae872891b370ec529
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2020-12-02 23:13:37 +0530

    handling card overflow error due to keyboard

--- lib/authorization/registration.dart
+++ lib/authorization/registration.dart
@@ -25,6 +25,7 @@ class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
