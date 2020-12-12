652946ce74b5a8f0ed29f7e3093ffa7e9c389ea5
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2020-12-13 00:33:42 +0530

    Completed Login Screen UI

--- lib/authorization/login.dart
+++ lib/authorization/login.dart
@@ -45,148 +45,132 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('images/fun.png'),fit: BoxFit.cover),
-            gradient: LinearGradient(
-                begin: Alignment.centerRight,
-                end: Alignment.bottomLeft,
-                colors: [
-              Color(0Xff294D64),
-              Color(0Xff294D64),
-            ])),
-        child: Card(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(35.0)),
-          margin: EdgeInsets.symmetric(horizontal: 40.0,vertical: 210.0),
-          elevation: 26.0,
-          shadowColor: Colors.black,
-          color: Color(0Xaa294D64).withOpacity(0.4),
-          child: ListView(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 20.0),
-                child: TextFormField(
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    labelText: "Enter Email",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    border:  OutlineInputBorder(
-                      borderRadius:  BorderRadius.circular(35.0),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                  validator: (val) {
-                    if(val.length==0) {
-                      return "Email cannot be empty";
-                    }else{
-                      return null;
-                    }
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
-                    fontFamily: "Poppins",
-                    color: Colors.white,
-                  ),
-                  onChanged: (value) {
-                    emailAddress = value;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: new InputDecoration(
-                    labelText: "Enter Password",
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    fillColor: Colors.white,
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(45.0),
-                      borderSide: new BorderSide(
-                      ),
-                    ),
-                  ),
-                  validator: (val) {
-                    if(val.length==0) {
-                      Fluttertoast.showToast(msg: 'Password cannot be empty',);
-                      return "Password cannot be empty";
-                    }else{
-                      return null;
-                    }
-                  },
-                  style: new TextStyle(
-                    fontFamily: "Poppins",
-                    color: Colors.white,
-                  ),
-                  onChanged: (value) {
-                    password = value;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 100.0,vertical: 50.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.centerRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Color(0Xff393e46),
-                          Color(0Xaa393e46),
-                        ]),
-                    borderRadius: BorderRadius.circular(30.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 2.0,
-                        spreadRadius: 0.0,
-                        offset:
-                        Offset(2.0, 2.0), // shadow direction: bottom right
-                      )
-                    ],
-                  ),
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: AutoSizeText('Login',
-                        maxLines: 1,
-                        style: GoogleFonts.montserrat(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w800,
-                          letterSpacing: 1.6,
-                          color: Colors.white,
-                        )),
-                    onPressed: () async {
-                      // The Function here logs users in with the help of Firebase Authorization.
-                      // )n the basis of isAdmin user is sent to the admin screen or the home screen using ternary statement.
-                      try {
-                        final newUser = await _auth.signInWithEmailAndPassword(
-                            email: emailAddress, password: password);
-                        if (newUser != null) {
-                          // print('Login Successful');
-                          await adminCheck();
-                          Navigator.pushNamed(
-                              context, isAdmin ? AdminScreen.id : HomeScreen.id);
-                        }
-                      } catch (e) {
-                        Fluttertoast.showToast(msg: e.toString(),);
-                      }
-                    },
-                  ),
-                ),
-              ),
-            ],
+            image: DecorationImage(image: AssetImage('images/Login.jpg'),fit: BoxFit.scaleDown,alignment: Alignment.lerp(Alignment.bottomCenter, Alignment.center, 0.35)),
+          color: Colors.white,
           ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 60,right:60,top: 100),
+              child: TextFormField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email,size: 18.0,color: Color(0Xff6B63FF),),
+                  labelText: "Enter Email",
+                  labelStyle: GoogleFonts.montserrat(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                validator: (val) {
+                  if(val.length==0) {
+                    return "Email cannot be empty";
+                  }else{
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                style: GoogleFonts.montserrat(
+                  color: Color(0Xff403D55),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+                onChanged: (value) {
+                  emailAddress = value;
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 60,right:60,top: 25),
+              child: TextFormField(
+                obscureText: true,
+                decoration: new InputDecoration(
+                  prefixIcon: Icon(Icons.vpn_key,size: 18.0,color: Color(0Xff6B63FF),),
+                  labelText: "Enter Password",
+                  labelStyle: GoogleFonts.montserrat(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                validator: (val) {
+                  if(val.length==0) {
+                    Fluttertoast.showToast(msg: 'Password cannot be empty',);
+                    return "Password cannot be empty";
+                  }else{
+                    return null;
+                  }
+                },
+                style: GoogleFonts.montserrat(
+                  color: Color(0Xff403D55),
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100.0,vertical: 50.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color(0Xff440047).withOpacity(0.7),
+                        Color(0Xff440047).withOpacity(0.8),
+                      ]),
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 2.0,
+                      spreadRadius: 0.0,
+                      offset:
+                      Offset(2.0, 2.0), // shadow direction: bottom right
+                    )
+                  ],
+                ),
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: AutoSizeText('Login',
+                      maxLines: 1,
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w800,
+                        letterSpacing: 1.6,
+                        color: Colors.white,
+                      )),
+                  onPressed: () async {
+                    // The Function here logs users in with the help of Firebase Authorization.
+                    // )n the basis of isAdmin user is sent to the admin screen or the home screen using ternary statement.
+                    try {
+                      final newUser = await _auth.signInWithEmailAndPassword(
+                          email: emailAddress, password: password);
+                      if (newUser != null) {
+                        // print('Login Successful');
+                        await adminCheck();
+                        Navigator.pushNamed(
+                            context, isAdmin ? AdminScreen.id : HomeScreen.id);
+                      }
+                    } catch (e) {
+                      Fluttertoast.showToast(msg: e.toString(),);
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
